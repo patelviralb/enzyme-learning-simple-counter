dee9bcf71a60711430c9b238771f17e126831efc
patelviralb <patel.viral@northeastern.edu>
2020-06-25 18:43:32 -0400

    Updated test cases

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+      error: false
+    }
+  }
+
+  incrementCounter = () => {
+    if (this.state.error) {
+      this.setState({
+        error: false
+      });
+    }
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+
+  decrementCounter = () => {
+    if (this.state.counter === 0) {
+      this.setState({ error: true });
+    } else {
+      this.setState({ ...this.state, counter: this.state.counter - 1 })
+    }
+  };
+
+  render() {
+    const errorDisplay = this.state.error ? '' : 'hidden';
+
+    return (
+      <div data-test="component-root">
+        <h1
+          data-test="counter-display">
+          Counter value is {this.state.counter}
+        </h1>
+        <h1
+          data-test="error-display"
+          className={`error ${errorDisplay}`}>
+          Cannot decrement counter further
+        </h1>
+        <button
+          data-test="increment-button"
+          onClick={this.incrementCounter}>
+          Increment counter
+        </button>
+        <button
+          data-test="decrement-button"
+          onClick={this.decrementCounter}>
+          Decrement counter</button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,119 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) {
+    wrapper.setState(state);
+  }
+  return wrapper;
+};
+
+const findByTestAttribute = (wrapper, value) => {
+  return wrapper.find(`[data-test="${value}"]`);
+}
+
+test('Render root element', () => {
+  const wrapper = setup();
+  const rootElement = findByTestAttribute(wrapper, "component-root");
+  expect(rootElement.length).toBe(1);
+});
+
+test('Render counter display', () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAttribute(wrapper, "counter-display");
+  expect(counterDisplay.length).toBe(1);
+});
+
+test('Render counter increment button', () => {
+  const wrapper = setup();
+  const incrementButton = findByTestAttribute(wrapper, "increment-button");
+  expect(incrementButton.length).toBe(1);
+});
+
+test('Render counter decrement button', () => {
+  const wrapper = setup();
+  const decrementButton = findByTestAttribute(wrapper, "decrement-button");
+  expect(decrementButton.length).toBe(1);
+});
+
+test('Counter start at 0', () => {
+  const wrapper = setup();
+  const initialState = wrapper.state("counter");
+  expect(initialState).toBe(0);
+});
+
+test('Clicking increment button increases the counter', () => {
+  const wrapper = setup({}, { counter: 3 });
+
+  const incrementButton = findByTestAttribute(wrapper, "increment-button");
+  incrementButton.simulate("click");
+
+  const counterDisplay = findByTestAttribute(wrapper, "counter-display");
+  expect(counterDisplay.text()).toContain(4);
+});
+
+test('Clicking decrement button decreases the counter', () => {
+  const wrapper = setup({}, { counter: 5 });
+
+  const decrementButton = findByTestAttribute(wrapper, "decrement-button");
+  decrementButton.simulate("click");
+
+  const counterDisplay = findByTestAttribute(wrapper, "counter-display");
+  expect(counterDisplay.text()).toContain(4);
+});
+
+test('Clicking decrement button at 0 doesn\'t decrease the counter', () => {
+  const wrapper = setup({}, { counter: 0 });
+
+  const decrementButton = findByTestAttribute(wrapper, "decrement-button");
+  decrementButton.simulate("click");
+
+  const counterDisplay = findByTestAttribute(wrapper, "counter-display");
+  expect(counterDisplay.text()).toContain(0);
+});
+
+test('Error text present', () => {
+  const wrapper = setup();
+  const renderError = findByTestAttribute(wrapper, "error-display");
+  expect(renderError.length).toBe(1);
+});
+
+test('Not showing error by default', () => {
+  const wrapper = setup();
+  const error = findByTestAttribute(wrapper, "error-display");
+
+  const errorHasHiddenClass = error.hasClass('hidden');
+  expect(errorHasHiddenClass).toBe(true);
+});
+
+describe("Decrement error functionality",() => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup();
+
+    const decrementButton = findByTestAttribute(wrapper, "decrement-button");
+    decrementButton.simulate("click");
+  })
+
+  test('Display error if counter is below 0', () => {
+    const error = findByTestAttribute(wrapper, "error-display");
+  
+    const errorHasHiddenClass = error.hasClass('hidden');
+    expect(errorHasHiddenClass).toBe(false);
+  });
+
+  test('Remove error if counter is incremented from 0', () => {
+    const incrementButton = findByTestAttribute(wrapper, "increment-button");
+    incrementButton.simulate("click");
+
+    const error = findByTestAttribute(wrapper, "error-display");
+  
+    const errorHasHiddenClass = error.hasClass('hidden');
+    expect(errorHasHiddenClass).toBe(true);
+  });
 });
